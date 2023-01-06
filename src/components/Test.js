45ef739a20a4ddabd9e9de6c28a4c2871f97f20e
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-01-06 23:16:30 +0300

    React Learning

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,23 +1,23 @@
 import './Test.css';
+import {useState} from 'react'
 
 let Test =({name, year})=>{
-    let clicked: false;
-    let following: false;
-    function onClicked (){
-        this.clicked = !clicked;
-    }
-    function onFollow(){
-        this.following = !following;
-    }
+    
+    const [clicked, setState]= useState(false)
+    // const [following, setState]=useState(false)
+    // onClick={()=>setState((following)=>!following)}>{following ? <p>Unfollow</p> : <p>Follow</p>}
+    
+    
     return (
         <div>
         <h1>Test React Component</h1>
         <h3>Prepared By:  {name} <br/> Year: {year}</h3>
 
         <i class="ri-message-3-line"></i>
-        {clicked ? <i class="ri-heart-2-fill" onClick={onClicked}></i>:<i class="ri-heart-3-line" onClick={onClicked}></i>}
+        {clicked ? <i class="ri-heart-2-fill" onClick={()=> setState((clicked)=>!clicked)}></i>:<i class="ri-heart-3-line" onClick={()=> setState((clicked)=>!clicked)}></i>}
 
-        <button type="submit" onClick={onFollow}>{following ? <p>Unfollow</p> :<p>Follow</p>}</button>
+        <button type="submit">Follow </button>
+        
         </div>
         );
 }
